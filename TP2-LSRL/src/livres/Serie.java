86eee840fa8a3905aa5eb53f7d1c51a15dd43e5d
la713447 <larasode203@gmail.com>
2026-03-23 13:28:58 -0400

    Ajout de classe Serie, un serie d'ouvrage, avec un nom et un arraylist listeOuvrage

---
+++ TP2-LSRL/src/livres/Serie.java
@@ -0,0 +1,11 @@
+package livres;
+
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Serie {
+public String nom;
+    private List<Ouvrage> listeOuvrage  = new ArrayList<>();
+}
