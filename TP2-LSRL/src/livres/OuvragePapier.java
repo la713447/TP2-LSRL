fb4887791cdc8557f56937c8eb053fd9b5a5e29d
la713447 <larasode203@gmail.com>
2026-03-27 18:27:21 -0400

    Changment de la signature de la classe OuvragePapier.java

--- TP2-LSRL/src/livres/OuvragePapier.java
+++ TP2-LSRL/src/livres/OuvragePapier.java
@@ -5,18 +5,17 @@ import java.time.LocalDate;
 public class OuvragePapier extends Ouvrage {
     private int nbPage;
 
-    public OuvragePapier(String titre, Auteur auteur, Format type, LocalDate date, int nombreExemplaires) {
+    public OuvragePapier(String titre, Auteur auteur, Ouvrage.Format type, LocalDate date, int nombreExemplaires) {
         super(titre, auteur, type, date, nombreExemplaires);
     }
 
-    public OuvragePapier(String titre, Auteur auteur, Format type) {
+    public OuvragePapier(String titre, Auteur auteur, Ouvrage.Format type) {
         super(titre, auteur, type);
     }
 
     public OuvragePapier(String titre, Auteur auteur) {
         super(titre, auteur);
     }
-
 
     public int getNbPage() {
         return nbPage;
