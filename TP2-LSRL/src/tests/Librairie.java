6405743d97c17754c645eb7567eef2e37d7dd562
la713447 <larasode203@gmail.com>
2026-03-27 17:02:45 -0400

    Modification de Librairie : remplacement des pays sans code par des objets Pays valides (CAN, USA, CHE)

--- TP2-LSRL/src/tests/Librairie.java
+++ TP2-LSRL/src/tests/Librairie.java
@@ -12,13 +12,22 @@ public class Librairie {
     private List<Ouvrage> ouvrages = new ArrayList<>();
 
     public Librairie(){
-        Auteur albertine = new Auteur("Albertine", "Tremblay", new Pays("Canada"));
-        Auteur john = new Auteur("John", "Tremblay", new Pays("Etats-unis"));
-        Auteur jean = new Auteur("Jean", "Némarre", new Pays("Suisse"));
+
+        // Création des pays avec codes valides
+        Pays canada = new Pays("Canada", "CAN");
+        Pays usa = new Pays("Etats-Unis", "USA");
+        Pays suisse = new Pays("Suisse", "CHE");
+
+        // Création des auteurs avec Pays
+        Auteur albertine = new Auteur("Albertine", "Tremblay", canada);
+        Auteur john = new Auteur("John", "Tremblay", usa);
+        Auteur jean = new Auteur("Jean", "Némarre", suisse);
+
         auteurs.add(albertine);
         auteurs.add(john);
         auteurs.add(jean);
 
+        // Création des ouvrages
         ouvrages.add(new Ouvrage("Titre 1", john));
         ouvrages.add(new Ouvrage("Titre 2", albertine));
         ouvrages.add(new Ouvrage("Titre 3", john));
@@ -26,8 +35,8 @@ public class Librairie {
         ouvrages.add(new Ouvrage("Titre 5", albertine));
         ouvrages.add(new Ouvrage("Titre 6", john));
         ouvrages.add(new Ouvrage("Titre 7", john));
-        ouvrages.add(new Ouvrage("Titre 8", albertine,Ouvrage.Format.VIDEO));
-        ouvrages.add(new Ouvrage("Titre 9", john,Ouvrage.Format.AUDIO));
+        ouvrages.add(new Ouvrage("Titre 8", albertine, Ouvrage.Format.VIDEO));
+        ouvrages.add(new Ouvrage("Titre 9", john, Ouvrage.Format.AUDIO));
         ouvrages.add(new Ouvrage("Titre 10", jean));
     }
 
@@ -40,10 +49,8 @@ public class Librairie {
     }
 
     public List<Ouvrage> trouverOuvrages(Auteur auteur) {
-        //ATTENTION il faut implémenter equals dans auteur pour que ca marche!!!!!
-        int count = 0;
-
         List<Ouvrage> trouves = new ArrayList<>();
+
         for (Ouvrage ouvrage : ouvrages) {
             if (ouvrage.getAuteur().equals(auteur)) {
                 trouves.add(ouvrage);
