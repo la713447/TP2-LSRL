d00f2c35895e697b7eefa017d9efa046e9ff9750
la713447 <larasode203@gmail.com>
2026-03-27 17:51:46 -0400

    Creation de la classe OuvragePapier.java

---
+++ TP2-LSRL/src/livres/OuvragePapier.java
@@ -0,0 +1,28 @@
+package livres;
+
+import java.time.LocalDate;
+
+public class OuvragePapier extends Ouvrage {
+    private int nbPage;
+
+    public OuvragePapier(String titre, Auteur auteur, Format type, LocalDate date, int nombreExemplaires) {
+        super(titre, auteur, type, date, nombreExemplaires);
+    }
+
+    public OuvragePapier(String titre, Auteur auteur, Format type) {
+        super(titre, auteur, type);
+    }
+
+    public OuvragePapier(String titre, Auteur auteur) {
+        super(titre, auteur);
+    }
+
+
+    public int getNbPage() {
+        return nbPage;
+    }
+
+    public void setNbPage(int nbPage) {
+        this.nbPage = nbPage;
+    }
+}
