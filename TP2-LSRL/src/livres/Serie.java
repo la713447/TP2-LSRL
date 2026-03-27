3efac756b1d5f4d6519f674a98f056196294567f
2544747 <2444747@cegeplimoilou.ca>
2026-03-27 17:23:45 -0400

    correction de la classe serie

--- TP2-LSRL/src/livres/Serie.java
+++ TP2-LSRL/src/livres/Serie.java
@@ -1,11 +1,61 @@
 package livres;
 
-
-import java.lang.reflect.Array;
 import java.util.ArrayList;
+
 import java.util.List;
 
 public class Serie {
-public String nom;
-    private List<Ouvrage> listeOuvrage  = new ArrayList<>();
+
+    public String nom;
+
+    private List<Ouvrage> listeOuvrage = new ArrayList<>();
+
+
+
+    public Serie(String nomSerie) {
+
+        this.nom = nomSerie;
+
+    }
+
+    public String getNom() {
+
+        return nom;
+
+    }
+
+    public List<Ouvrage> getListeOuvrage() {
+
+        return listeOuvrage;
+
+    }
+
+    public void setNom(String nom) {
+
+        this.nom = nom;
+
+    }
+
+    public void setListeOuvrage(List<Ouvrage> listeOuvrage) {
+
+        this.listeOuvrage = listeOuvrage;
+
+    }
+
+
+
+    public void ajouterOuvrage(Ouvrage o) {
+
+        listeOuvrage.add(o);
+
+    }
+
+
+
+    public void enleverOuvrage(Ouvrage o) {
+
+        listeOuvrage.remove(o);
+
+    }
+
 }
